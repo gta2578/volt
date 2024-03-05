0d6318b9a10568810abccc0932c34e945e8816a4
gta2578 <gta2578@gmail.com>
2024-03-05 23:37:52 +0200

    improvement: new event handler

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -21,11 +21,19 @@ class AddTodo extends React.Component {
     }
   };
 
+  handleKeyPress(e){
+    const key=e.keyCode || e.which;
+    if (key === 13){
+      this.handleAddTodo();
+    }
+  }
+
   render() {
     return (
       <div className="input-wrapper">
         <input
           onChange={e => this.updateInput(e.target.value)}
+          onKeyPress={e => this.handleKeyPress(e)}
           value={this.state.input}
           placeholder='Enter text (max - 100 characters)'
         />
